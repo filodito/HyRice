f8b211d4fa374e4673fe0344b15d95ea0ab0c200
filodito <archyrep25@outlook.com>
2025-08-31 21:58:07 +0200

    Delete pywal/CherryBlossom directory

--- pywal/CherryBlossom/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c3c2c1", "#110b07" },
-	[SchemeSel] = { "#c3c2c1", "#B6A48A" },
-	[SchemeOut] = { "#c3c2c1", "#E9DDC3" },
-};
